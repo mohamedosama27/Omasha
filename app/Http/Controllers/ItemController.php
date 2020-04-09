ba6cee1d4be35f6a73e7e1f019ba5cbccc4c0fe9
mohamed osama <mohamed1705725@miuegypt.edu.eg>
2020-04-09 21:08:25 +0200

    herouk changes3

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -17,9 +17,13 @@ class ItemController extends Controller
     protected $items_per_page = 10;
 
     public function index(Request $request) {
+      if(\App\item::count()>10){
 
         $items = \App\item::paginate($this->items_per_page);
-
+      }
+      else{
+        $items = \App\item::paginate(\App\item::count());
+      }
         if($request->ajax()) {
             return [
                 'items' => view('ajax.index')->with(compact('items'))->render(),
