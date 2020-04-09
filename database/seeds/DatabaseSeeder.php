cc9ea2ee39e3dc9bedf51dd49c40713aa2199de7
mohamed osama <mohamed1705725@miuegypt.edu.eg>
2020-04-09 16:36:51 +0200

    heroku change2

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,6 +16,6 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         // factory(image::class,80)->create();
-        factory(User::class,100)->create();
+        factory(User::class,1)->create();
     }
 }
