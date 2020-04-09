ae09399bdc9786212b00c2e1ec3e826de55ba26c
mohamed osama <mohamed1705725@miuegypt.edu.eg>
2020-04-09 22:30:53 +0200

    herouk changes3

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            
+            $table->string('phone');
+            $table->boolean('type');
             $table->rememberToken();
             $table->timestamps();
         });
--- database/migrations/2020_04_09_202021_create_messages_table.php
+++ database/migrations/2020_04_09_202021_create_messages_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateOrdersTable extends Migration
+class CreateMessagesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,12 @@ class CreateOrdersTable extends Migration
      */
     public function up()
     {
-        Schema::create('orders', function (Blueprint $table) {
+        Schema::create('messages', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->unsignedBigInteger('user_id');
-            $table->text('address');
-            $table->text('total_price')->nullable();
-            $table->text('status');
+            $table->unsignedBigInteger('sender_id');
+            $table->unsignedBigInteger('recivier_id');
+            $table->string('message');
+            $table->boolean('status');
             $table->timestamps();
         });
     }
@@ -30,6 +30,6 @@ class CreateOrdersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('orders');
+        Schema::dropIfExists('messages');
     }
 }
