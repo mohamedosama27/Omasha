5238fba8786c1d04cb44a12a3a0a9aa514c6dd2b
mohamed osama <mohamed1705725@miuegypt.edu.eg>
2020-04-11 20:23:48 +0200

    herouk changes3

--- resources/views/bar.blade.php
+++ resources/views/bar.blade.php
@@ -5,6 +5,8 @@
 <meta charset="UTF-8">
 
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content= "width=device-width, user-scalable=no">
+
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
