e7c4e5aa884df9d94d8788bb7b3ef35bc0ff62e2
mohamed osama <mohamed1705725@miuegypt.edu.eg>
2020-04-14 16:38:35 +0200

    heroku change2

--- resources/views/chats.blade.php
+++ resources/views/chats.blade.php
@@ -47,7 +47,13 @@
  </div>
  <script type="text/javascript">
 
-   
+window.onpageshow = function(evt) {
+    // If persisted then it is in the page cache, force a reload of the page.
+    if (evt.persisted) {
+        document.body.style.display = "none";
+        location.reload();
+    }
+};
 
     $.ajaxSetup({
 
