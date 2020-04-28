8e74d58192eaadc92f645d6ed6dd9d50d7909585
mohamed osama <mohamed1705725@miuegypt.edu.eg>
2020-04-28 17:44:17 +0200

    changes

--- resources/views/bar.blade.php
+++ resources/views/bar.blade.php
@@ -17,6 +17,10 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  
+  <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"> </script>
+
 <style>
 .w3-sidebar a {font-family: "Roboto", sans-serif}
 a{
