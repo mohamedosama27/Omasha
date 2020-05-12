4b21aba55712448aedac5e612e9689ac68b700ae
mohamed osama <mohamed1705725@miuegypt.edu.eg>
2020-05-12 23:32:50 +0200

    changes

--- database/factories/imagefactory.php
+++ database/factories/imagefactory.php
@@ -8,11 +8,15 @@ use App\item;
 use Faker\Generator as Faker;
 
 $factory->define(image::class, function (Faker $faker) {
-    $image=image::all()->random(1);
+    $images=['11172.png','11355.jpeg','14121.jpeg','17057.jpeg','20468.jpeg','93106.jpeg','31240.jpeg'];
     $item=item::all()->random(1);
-
+        // get random index from array $arrX
+    $randIndex = array_rand($images);
+    
+    // output the value for the random index
+     
     return [
-        'name'=>$image[0]->name,
+        'name'=>$images[$randIndex],
         'item_id' => $item[0]->id,
 
     ];
