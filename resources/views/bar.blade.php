ef31b40df75392ebbab6e7963d29fa0b1a77ef1e
mohamed osama <mohamed1705725@miuegypt.edu.eg>
2020-06-20 23:58:02 +0200

    changes

--- resources/views/bar.blade.php
+++ resources/views/bar.blade.php
@@ -5,7 +5,7 @@
 <title>OMASHA</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="viewport" content= "width=device-width, user-scalable=no">
+
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="csrf-token" content="{{ csrf_token() }}" />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -28,7 +28,6 @@
 @endif
 @endauth
 <body>
-<div class="wrapper body"> 
 <!-- Start Search Modal -->
 <div class="modal fade" id="searchModel" role="dialog">
   <div class="modal-dialog modal-lg">
@@ -485,7 +484,6 @@
   </div>
   
 </section>
-</div>
 </body>
 <script>
  $('#searchInput').keyup(function(){ 
