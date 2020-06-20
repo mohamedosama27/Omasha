82baff22521b58c899b5542156157bce161d68b5
mohamed osama <mohamed1705725@miuegypt.edu.eg>
2020-06-21 00:13:10 +0200

    changes

--- resources/views/bar.blade.php
+++ resources/views/bar.blade.php
@@ -5,7 +5,7 @@
 <title>OMASHA</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-
+<meta name="viewport" content= "width=device-width, user-scalable=no">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="csrf-token" content="{{ csrf_token() }}" />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
